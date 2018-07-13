f26291581b8a6de88199dd9853ecc6919952577a
a632079 <a632079@qq.com>
2018-07-14 00:44:56 +0800

    First Commit

---
+++ Newbe.Mahua.Plugins.Template.MPQ1/settingForm.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Newbe.Mahua.Plugins.Template.MPQ1
+{
+    partial class settingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "监控 URI";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(216, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "https://status.hitokoto.cn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "监控周期 (毫秒)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(216, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(355, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "500";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(494, 144);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(77, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "25";
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(216, 188);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(355, 22);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(216, 241);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.PasswordChar = '*';
+            this.textBox5.Size = new System.Drawing.Size(355, 22);
+            this.textBox5.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(428, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "端口";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(43, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "用户名";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(43, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "密码";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("微软雅黑", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(379, 304);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(192, 30);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "启用 SSL 加密连接";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(43, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "SMTP 域名";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(216, 144);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(203, 22);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("微软雅黑", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(216, 370);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(355, 84);
+            this.textBox7.TabIndex = 14;
+            this.textBox7.Text = "请使用英文半角 \",\" 分割。 示例: a632079@qq.com,i@loli.online,i@freejishu.com";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(43, 370);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 25);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "通知邮箱";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("微软雅黑", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(216, 475);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(355, 84);
+            this.textBox8.TabIndex = 16;
+            this.textBox8.Text = "请使用英文半角 \",\" 分割。 示例: 691759538,473589715,70029304";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(43, 475);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 25);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "通知群组";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(48, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 40);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "测试 SMTP 链接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(352, 588);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 40);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "保存";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // settingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 656);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "settingForm";
+            this.Text = "一言状态监控 - 配置中心";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Newbe.Mahua.Plugins.Template.MPQ1/settingForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IniParser;
+using IniParser.Model;
+using System.Net;
+using System.Net.Mail;
+
+namespace Newbe.Mahua.Plugins.Template.MPQ1 {
+    public partial class settingForm : Form {
+        static private string workDir = System.AppDomain.CurrentDomain.BaseDirectory; // 获取程序目录
+        static private string pluginConfigDir = workDir + "/Plugin/Config";
+        static private string configFile = pluginConfigDir + "/statusReport.settings.ini";
+
+        public settingForm() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // 窗口载入 读配置项
+            if (!Directory.Exists(pluginConfigDir)) {
+                Directory.CreateDirectory(pluginConfigDir);
+            }
+
+            if (File.Exists(configFile)) {
+                // 初始化解析器
+                var parser = new FileIniDataParser();
+                try {
+                    IniData data = parser.ReadFile(configFile);
+                    // 导入配置
+                    textBox1.Text = data["monitor"]["uri"] != "" ? data["monitor"]["uri"] : textBox1.Text;
+                    textBox2.Text = data["monitor"]["tickTime"] != "" ? data["monitor"]["tickTime"] : textBox2.Text;
+
+                    textBox3.Text = data["smtp"]["port"] != "" ? data["smtp"]["port"] : textBox3.Text;
+                    textBox4.Text = data["smtp"]["username"] != "" ? data["smtp"]["username"] : textBox4.Text;
+                    textBox5.Text = data["smtp"]["password"] != "" ? data["smtp"]["password"] : textBox5.Text;
+                    textBox6.Text = data["smtp"]["host"] != "" ? data["smtp"]["host"] : textBox6.Text;
+                    checkBox1.Checked = data["smtp"]["enableSSL"] != "" ? bool.Parse(data["smtp"]["enableSSL"]) : checkBox1.Checked;
+
+                    textBox7.Text = data["notification"]["mail"] != "" ? data["notification"]["mail"] : textBox7.Text;
+                    textBox8.Text = data["notification"]["group"] != "" ? data["notification"]["group"] : textBox8.Text;
+
+                } catch (InvalidCastException err) { // 说是捕获错误， 其实是丢掉所有错误
+                    Console.WriteLine(err);
+                }
+            }
+        }
+
+        private void saveConfig() {
+            if (!Directory.Exists(pluginConfigDir)) {
+                Directory.CreateDirectory(pluginConfigDir);
+            }
+
+            if (!File.Exists(configFile)) {
+                File.Create(configFile);
+            }
+
+            // 保存配置到配置项文件
+            // 初始化解析器
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile(configFile);
+
+            // 监控配置
+            data["monitor"]["uri"] = textBox1.Text;
+            data["monitor"]["tickTime"] = textBox2.Text;
+
+            // SMTP 配置
+            data["smtp"]["host"] = textBox6.Text;
+            data["smtp"]["port"] = textBox3.Text;
+            data["smtp"]["username"] = textBox4.Text;
+            data["smtp"]["password"] = textBox5.Text;
+            data["smtp"]["enableSSL"] = checkBox1.Checked.ToString();
+
+            // 通知配置
+            data["notification"]["mail"] = textBox7.Text;
+            data["notification"]["group"] = textBox8.Text;
+
+            // 写入
+            parser.WriteFile(configFile, data);
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            saveConfig();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            // 测试 SMTP 链接
+            // 映射信息
+            string host = textBox6.Text;
+            int port = int.Parse(textBox3.Text);
+            string username = textBox4.Text;
+            string password = textBox5.Text;
+            bool enableSSL = checkBox1.Checked;
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(username);
+            mail.To.Add("i@a632079.me");
+            mail.Subject = "Test Connection";
+            mail.Body = "Just Test Mail Connection";
+            mail.IsBodyHtml = false;
+            try {
+                SmtpClient smtp = new SmtpClient(host, port);
+                smtp.Credentials = new NetworkCredential(username, password);
+                smtp.EnableSsl = enableSSL;
+                smtp.Send(mail);
+                MessageBox.Show("连接成功");
+            } catch (ArgumentException err) {
+                MessageBox.Show("连接失败， 错误信息请查看错误堆栈！");
+                throw err;
+            } catch (Exception err) {
+                MessageBox.Show("连接失败， 错误信息请查看错误堆栈！");
+                throw err;
+            }
+
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e) {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.')) {
+                e.Handled = true;
+            }
+        }
+    }
+}
